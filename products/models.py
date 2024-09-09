f480f163d0b13f92fe7530eea0096715ff7716d8
DESKTOP-NK05NPD\User <gyum9779@naver.com>
2024-09-10 00:31:17 +0900

    rough Products model

--- products/models.py
+++ products/models.py
@@ -1,19 +1,34 @@
 from django.db import models
+from accounts.models import User
 
 # Create your models here.
 
 
-class Product(models.Model):
-    CATEGORY_CHOICES = (
-        ("F", "Fruit"),
-        ("V", "Vegetable"),
-        ("M", "Meat"),
-        ("O", "Other"),
-    )
-    name = models.CharField(max_length=30)
-    price = models.PositiveIntegerField()
-    quantity = models.PositiveIntegerField()
-    category = models.CharField(max_length=100)
+class TimeStampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-        return self.name
+    class Meta:
+        abstract = True
+
+
+class Product(TimeStampedModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=30)
+    description = models.TextField()
+
+
+# class Product(models.Model):
+#     CATEGORY_CHOICES = (
+#         ("F", "Fruit"),
+#         ("V", "Vegetable"),
+#         ("M", "Meat"),
+#         ("O", "Other"),
+#     )
+#     name = models.CharField(max_length=30)
+#     price = models.PositiveIntegerField()
+#     quantity = models.PositiveIntegerField()
+#     category = models.CharField(max_length=100)
+#
+#     def __str__(self):
+#         return self.name
