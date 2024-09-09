4943635597466fb543e818c7d245988eb7d88b51
DESKTOP-NK05NPD\User <gyum9779@naver.com>
2024-09-10 01:01:23 +0900

    APIView - Product Detail

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
-from .views import ProductListAPIView
+from .views import ProductListAPIView, ProductDetailAPIView
 
 urlpatterns = [
     path("", views.ProductListAPIView.as_view(), name="product_list"),
+    path("<int:pk>/", views.ProductDetailAPIView.as_view(), name="product_detail"),
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -17,3 +17,26 @@ class ProductListAPIView(APIView):
         if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class ProductDetailAPIView(APIView):
+    def get_object(self, pk):
+        return get_object_or_404(Product, pk=pk)
+
+    def get(self, request, pk):
+        product = self.get_object(pk)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        product = self.get_object(pk)
+        serializer = ProductSerializer(product, data=request.data, partial=True)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+
+    def delete(self, request, pk):
+        product = self.get_object(pk)
+        product.delete()
+        data = {"pk": f"{pk} is deleted."}
+        return Response(data, status=status.HTTP_200_OK)
